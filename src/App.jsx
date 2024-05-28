dfa9ccd19fb22cda3d2b4c8b5345af811cba4e25
Ezequiel Amin <ezequielamin@outlook.com>
2024-05-28 16:16:09 -0300

    agrego cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import CardsGallery from './components/CardsGallery/CardsGallery';
 
-function App() {
-  const [count, setCount] = useState(0)
-  const mensaje = "Hola mundo"
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{mensaje}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hola mundo!</h1>
+      <CardsGallery />
     </>
-  )
-}
-
-export default App
+  );
+};
+export default App;
