8f1b665f82f43923b62259160ff08cf3b69c08d1
Ezequiel Amin <ezequielamin@outlook.com>
2024-06-11 15:07:11 -0300

    agrego react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './constants/routes.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/views/ContactsView.jsx
@@ -0,0 +1,6 @@
+const ContactsView = () => {
+  return (
+    <div>Esta es mi vista de contactos lol xd</div>
+  )
+}
+export default ContactsView
